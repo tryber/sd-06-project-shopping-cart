553ffcc372313b441e5f167117e5a8d22d3b72e6
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-31 20:04:07 -0300

    Implementação requisito 1 e correção do 6

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function addToCart(event) {
+  const monitorScreen = event.target;
+  const sku = monitorScreen.parentNode.firstChild.innerHTML;
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(response => response.json())
+    .then((data) => {
+      const newItem = {
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      };
+      const itemsList = document.querySelector('.cart__items');
+      itemsList.appendChild(createCartItemElement(newItem));
+    });
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,8 +35,8 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', addToCart);
   return section;
 }
 
@@ -43,7 +59,7 @@ function createCartItemElement({ sku, name, salePrice }) {
 // requisito 6 Botão Limpar carrinho de compras
 function clearAll() {
   console.log('Apagado!');
-  document.getElementsByClassName('cart__items').innerHTML = '';
+  document.querySelector('.cart__items').innerHTML = '';
   localStorage.clear();
 }
 
@@ -56,7 +72,7 @@ const fetchProduct = (url) => {
           sku: item.id,
           name: item.title,
           image: item.thumbnail,
-        });
+        });// usa o forEach para criar a lsta conforme a função prédeterminada
         document.querySelector('.items').appendChild(products);
         document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('salvedItens');
       });
