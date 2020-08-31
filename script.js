8ff3739901fa67f92fc9588cea244243d67575c1
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-31 15:44:20 -0300

    1rst requirement

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.onload = function onload() { };
+function fetchProducts() {
+  const searchValue = 'computador'
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=$${searchValue}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((object) => {
+      const products = object.results;
+      products.forEach(element => {
+        const product = createProductItemElement({
+          sku: element.id,
+          name: element.title,
+          image: element.thumbnail,
+        });
+        document.querySelector('.items').appendChild(product);
+      });
+    });
+}
+// nesta funcao pegamos a url da API e damos um fetch
+// pegamos o objeto e o transformamos em algo legivel com o .json
+// com o forEach, em cada elemento criamos um objeto novo
+// com sua sku (id), name(title) e image(thumbnail)
+// e depois fazemos um appendchild na secao 'items' do html
+// a funcao que chamamos createProductItemElement ja cria
+// a secao de cada item especifico com a classe 'item'
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -17,13 +41,16 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  const itemsSection = document.querySelector('.items');
+
+  itemsSection.appendChild(section);
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
+
 }
 
 function getSkuFromProductItem(item) {
@@ -41,3 +68,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  fetchProducts();
+};
