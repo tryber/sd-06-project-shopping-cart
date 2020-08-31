5c4893d0650726f266d948bb045ab05301840686
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-31 20:31:42 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.onload = function onload() { };
+window.onload = function onload() { 
+  fetchComputers();
+};
+
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +18,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +30,30 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqu
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqu
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+function fetchComputers () {
+  const section = document.querySelector('.items');
+
+  fetch(url)
+    .then(response => response.json())
+    .then(response => response.results)
+    .then(arrayOfComputers => arrayOfComputers.forEach(computer => {
+      const product = createProductItemElement(computer);
+      section.appendChild(product);
+    }));
 }
