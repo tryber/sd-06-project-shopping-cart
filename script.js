f2018a17270704f0eaf44d2604fbba9016d2ee19
Micaeli Teixeira <luime6@gmail.com>
2020-08-31 17:51:25 -0300

    Criando listagem de produtos vindos da API

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.onload = function onload() { };
+// pegando a API
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +15,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// é chamada pela função filterElementsObjectApi
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +28,42 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+// função para olhar dentro do object.result e criar os subitens de .items
+const filterElementsObjectApi = (elementsArray) => {
+  elementsArray.forEach((element) => {
+    const arrayProducts = document.querySelector('.items');
+    arrayProducts.appendChild(createProductItemElement(element));
+  });
+};
+
+// função para chamar a API
+const handleAPI = () => {
+  fetch(url)
+  .then(response => response.json())
+  .then((object) => {
+    console.log(object);
+    const objectNew = object.results;
+    console.log(object.results);
+    filterElementsObjectApi(objectNew);
+  });
+};
+
+window.onload = function onload() {
+  handleAPI();
+};
