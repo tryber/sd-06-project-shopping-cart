974ed884a4003fd2015f66842fbf079fb4184e6b
Anderson <andersoni.trybe@gmail.com>
2020-08-31 20:37:59 -0300

    Structural review for CodeClimate

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const thisButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  thisButton.addEventListener('click', () => queryThisAtML(sku));
+  section.appendChild(thisButton);
+
+  return section;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -44,33 +67,11 @@ function queryThisAtML(sku) {
       const Item = createCartItemElement({
         sku: response.id,
         name: response.title,
-        salePrice: response.price, 
+        salePrice: response.price,
       });
       cartItems.appendChild(Item);
     });
   return queryUrl;
-}
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  const thisButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  thisButton.addEventListener('click', () => queryThisAtML(sku));
-  section.appendChild(thisButton);
-
-  return section;
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 window.onload = function onload() {
