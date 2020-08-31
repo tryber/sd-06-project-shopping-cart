99013899ccf7a0a843e43207aea2cc72168c3a4e
DekoPossas <andrepossaster@gmail.com>
2020-08-31 19:08:52 -0300

    Implemeted website requirement, creating itens in the showcase of we site

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+
+// criei uma URL com o endPoint do mercado livre
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -26,18 +28,50 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// Criei a função que faz a requisição no site URL.
+const fetchWindow = () => {
+  // criei uma variavel e atribui a ela a url do endpoint do ML
+  const endPoint = `${url}`;
+  // criei um fatch na URL digitando aqui agora eu ate questiono se é necessario
+  // os passos anteriores, talvez eu mexa no futuro.
+  fetch(endPoint)
+    // metodo then para receber a promisse binario e transformar em um obj
+    .then(response => response.json())
+    // metodo then que recebe o obj e transforma em um array ja no metodo
+    // foreach que vai criar um obj pra cada outro obj recebido da response
+    .then(object => object.results.forEach((item) => {
+      const obj = {
+        sku: item.id,
+        name: item.title,
+        image: item.thumbnail,
+      };
+      // recolhi a section com a class 'items' para poder
+      // adicionar filhos a ela
+      const showcase = document.querySelector('.items');
+      showcase.appendChild(createProductItemElement(obj));
+    }));
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// atribui ao meu script uma function que dispara
+// functions assim que a pagina esta carregada
+window.onload = function onload() {
+  // disparei a function fetchWindow que na verdade é uma requisição
+  // do API do mercado livre.
+  fetchWindow();
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
