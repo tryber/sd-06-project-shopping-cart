d025e4b09d79deb5578f691a924b4b9ae6d47475
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-31 16:47:04 -0300

    Resolução do requisito 01

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,18 +24,39 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const showProducts = (results) => {
+  const itemProduct = document.querySelector('.items');
+  results.forEach((result) => {
+    myObject = { sku: result.id, name: result.title, image: result.thumbnail };
+    itemProduct.appendChild(createProductItemElement(myObject));
+  });
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const fetchProducts = (myApi) => {
+  fetch(myApi)
+    .then(response => response.json())
+    .then((object) => {
+      showProducts(object.results);
+    });
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+window.onload = function onload() {
+  const myApi = 'https://api.mercadolibre.com/sites/MLB/search?q=$computadores';
+  fetchProducts(myApi);
+};
