52f37d66c4a6dac43401b4769c2096167e638b5a
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-31 18:39:00 -0300

    01- primeiro requisito

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -17,12 +17,13 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  const productSection = document.querySelector('.items');
+  productSection.appendChild(section);
   return section;
 }
 
@@ -41,3 +42,25 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// Função para pegar resposta da api
+const fetchUrl = () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(url)
+  .then(response => response.json())
+  // .then(object => console.log(object.results));
+  .then((data) => {
+    data.results.forEach((cpu) => {
+      const finale = {
+        sku: cpu.id,
+        name: cpu.title,
+        image: cpu.thumbnail,
+      };
+      createProductItemElement(finale);
+    });
+  });
+};
+
+window.onload = function onload() {
+  fetchUrl();
+};
