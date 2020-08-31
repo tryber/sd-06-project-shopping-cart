7efcc519f4a90e8b9e907e677f9465a883ea38c5
William <williamjog@hotmail.com>
2020-08-31 16:25:10 -0300

    2nd requirement after 1 century

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener() {
+  console.log('EVENTO DE CLICK OK');
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+
+  return li;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+
   return e;
 }
 
@@ -20,39 +38,40 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(data => data.json())
+    .then((data) => {
+      console.log(data);
+      const item = createCartItemElement({
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      });
+      document.querySelector('.cart__items').appendChild(item);
+    });
+  });
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+const fetchList = () => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then(response => response.json())
+  .then((response) => {
+    console.log(response.results);
+    response.results.forEach((item) => {
+      const product = createProductItemElement({
+        sku: item.id,
+        name: item.title,
+        image: item.thumbnail,
+      });
+      document.querySelector('.items').appendChild(product);
+    });
+  });
+};
 
 window.onload = function onload() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then((response) => {
-      console.log(response.results);
-      response.results.forEach((item) => {
-        const product = createProductItemElement({
-          sku: item.id,
-          name: item.title,
-          image: item.thumbnail,
-        });
-        document.querySelector('.items').appendChild(product);
-      });
-    });
+  fetchList();
 };
