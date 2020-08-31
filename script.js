55f0395fe96d9690b918c43c27d6018634eb54b1
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-31 17:07:34 -0300

    2nd requirement

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  const mainList = document.querySelector('.cart__items');
+  mainList.removeChild(event.target);
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,24 +41,21 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(object => object.json())
+    .then((object) => {
+      const item = createCartItemElement({
+        sku: object.id,
+        name: object.title,
+        salePrice: object.price,
+      });
+      const cart = document.querySelector('.cart__items');
+      cart.appendChild(item);
+    });
+  });
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function fetchProducts() {
@@ -62,12 +77,13 @@ function fetchProducts() {
     });
 }
 // nesta funcao pegamos a url da API e damos um fetch
-// pegamos o objeto e o transformamos em algo legivel com o .json
+// pegamos o objeto da resposta e o transformamos em algo legivel com o .json
 // com o forEach, em cada elemento criamos um objeto novo
 // com sua sku (id), name(title) e image(thumbnail)
 // e depois fazemos um appendchild na secao 'items' do html
 // a funcao que chamamos createProductItemElement ja cria
 // a secao de cada item especifico com a classe 'item'
+
 
 window.onload = function onload() {
   fetchProducts();
