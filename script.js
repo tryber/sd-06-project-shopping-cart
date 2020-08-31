f69d29715ca2cf05290757c42ca31edd0cee3855
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-08-31 19:38:48 -0300

    revisão code climate tarefa 01
    revisão code climate tarefa 01

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-window.onload = function onload() {
-  const URL = `${apiURL.url}${apiURL.endPoint}${apiURL.search}`;
-  fetch(URL)
-    .then(response => response.json())
-    .then((resultJSON) => {
-      const result = resultJSON.results;
-      listReturned(result);
-    })
-    .catch(error => window.alert(error));
-};
-
 const apiURL = {
   url: 'https://api.mercadolibre.com/',
   endPoint: 'sites/MLB/search?q=',
-  search: 'computador'
-}
-
-function listReturned(arrOfProducts) {
-  arrOfProducts.forEach((element) => {
-    const secItems = document.querySelector('.items');
-    secItems.appendChild(createProductItemElement(element.id, element.title, element.thumbnail))
-  });
-
-}
-
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
+  search: 'computador',
+};
 
 function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
@@ -48,15 +15,47 @@ function createProductItemElement(sku, name, image) {
 
   console.log(section);
   return section;
-}
+};
+
+function listReturned(arrOfProducts) {
+  arrOfProducts.forEach((element) => {
+    const secItems = document.querySelector('.items');
+    secItems.appendChild(createProductItemElement(element.id, element.title, element.thumbnail))
+  });
+};
+
+window.onload = function onload() {
+  const URL = `${apiURL.url}${apiURL.endPoint}${apiURL.search}`;
+  fetch(URL)
+    .then(response => response.json())
+    .then((resultJSON) => {
+      const result = resultJSON.results;
+      listReturned(result);
+    })
+    .catch(error => window.alert(error));
+};
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+};
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
