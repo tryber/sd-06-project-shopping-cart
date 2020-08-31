df928bbbaf04fe170ab85bb7724729d3694e3a37
Isadora Koga <isadorakoga710@gmail.com>
2020-08-31 16:27:16 -0300

    Open PR // Página renderizando os produtos e criando os itens

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.onload = function onload() { };
+const apiInfo = {
+  api: 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+}
+
+const url = apiInfo.api;
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +18,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,10 +38,34 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function renderItem(arrayProducts) {
+  arrayProducts.forEach(product => {
+    const newItem = createProductItemElement(product)
+    const section = document.querySelector('.items');
+    section.appendChild(newItem);
+  });
+}
+
+const getApi = () => {
+  const endpoint = url;
+  
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((object) => {
+      console.log(object.results);
+      const result = object.results;
+      renderItem(result);
+    });
+}
+
+window.onload = function onload() {
+  getApi();
+ };
