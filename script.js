d3558b83b808b5531a77664537e38a58ead00751
Isadora Koga <isadorakoga710@gmail.com>
2020-08-31 16:34:43 -0300

    CC ajustado, página renderizando itens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiInfo = {
-  api: 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
-}
+  api: 'https://api.mercadolibre.com/sites/MLB/search?q=computador',
+};
 
 const url = apiInfo.api;
 
@@ -30,25 +30,25 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 function renderItem(arrayProducts) {
-  arrayProducts.forEach(product => {
-    const newItem = createProductItemElement(product)
+  arrayProducts.forEach((product) => {
+    const newItem = createProductItemElement(product);
     const section = document.querySelector('.items');
     section.appendChild(newItem);
   });
@@ -56,16 +56,16 @@ function renderItem(arrayProducts) {
 
 const getApi = () => {
   const endpoint = url;
-  
+
   fetch(endpoint)
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((object) => {
       console.log(object.results);
       const result = object.results;
       renderItem(result);
     });
-}
+};
 
 window.onload = function onload() {
   getApi();
- };
+};
