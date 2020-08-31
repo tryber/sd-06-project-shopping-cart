5249f1af41e14b674a8e3b1ec526efb4d962f5f7
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-31 20:09:59 -0300

    Acerto CC

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -50,10 +54,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 // requisito 6 Botão Limpar carrinho de compras
