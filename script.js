c8d14f78c1f6f829174c84df75d244fc35621646
ricknois <rickssti@outlook.com>
2020-08-31 18:29:36 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +26,40 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui.
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui.
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+// function createCartItemElement({sku, name, salePrice}) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const apiInfo = {
+  api: 'https://api.mercadolibre.com/sites/MLB/',
+  endpoint: 'search?q=computador',
+};
+
+const url = `${apiInfo.api}${apiInfo.endpoint}`;
+
+function computerFetch() {
+  const endpoint = url;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((obj) => {
+      obj.results.forEach((elem) => {
+        const secItems = document.querySelector('.items');
+        const createItemChild = createProductItemElement(elem.id, elem.title, elem.thumbnail);
+        secItems.appendChild(createItemChild);
+        // console.log(elem.thumbnail);
+      });
+    });
 }
+window.onload = function onload() { computerFetch()};
