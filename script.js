2992274b2f802cf1e3828214ef9da13cd58ef9b6
Daniele Perse <danieleperse3@gmail.com>
2020-08-31 17:07:16 -0300

    requisito numero 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +14,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +27,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+}*/
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+}*/
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}*/
+
+function fetchProduct() {
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      data.results.forEach((product) => {
+        const section = document.querySelector('.items');
+        const newProduct = createProductItemElement(product);
+        section.appendChild(newProduct);
+      });
+    });
 }
+
+window.onload = function onload() {
+  fetchProduct();
+};
