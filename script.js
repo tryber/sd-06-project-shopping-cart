df2e2aa0a109535d3cb1473ef46edd550148de55
LughWalle <civalfran1@gmail.com>
2020-08-31 13:17:07 -0500

    first commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
+  
 }
 
 function createProductItemElement({ sku, name, image }) {
