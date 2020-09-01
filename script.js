e75d6ccb6c7f0888363b0f95c7f2ceaf52487555
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-01 13:56:51 -0300

    issues req 2

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
 
   return li;
 }
