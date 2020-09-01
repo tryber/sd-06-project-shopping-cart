817e41fd5f867d9d2cf208349bcf37e5a59ce4fe
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-01 17:20:27 -0300

    Requisito 2.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable arrow-parens */
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -32,7 +34,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -44,17 +46,34 @@ function createItemList() {
   const apiList = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   fetch(apiList).then(response => response.json()).then((data) => {
     data.results.forEach((computer) => {
-      const computerEdited = {
-        sku: computer.id,
-        name: computer.title,
-        image: computer.thumbnail,
-      };
       const itemList = document.querySelector('.items');
-      itemList.appendChild(createProductItemElement(computerEdited));
+      itemList.appendChild(createProductItemElement(computer));
     });
   });
 }
 
+function fetchItem(itemID) {
+  const apiItem = `https://api.mercadolibre.com/items/${itemID}`;
+  return fetch(apiItem).then(response => response.json());
+}
+
+function addItemToCart(eventElement) {
+  if (eventElement.className === 'item__add') {
+    const selectedItem = eventElement.parentNode;
+    const itemID = getSkuFromProductItem(selectedItem);
+    fetchItem(itemID)
+      .then((data) => {
+        const cartList = document.querySelector('.cart__items');
+        cartList.appendChild(createCartItemElement(data));
+      });
+  }
+}
+
 window.onload = function onload() {
   createItemList();
+  const addButton = document.querySelector('.items');
+  addButton.addEventListener('click', () => {
+    const button = event.target;
+    addItemToCart(button);
+  });
 };
