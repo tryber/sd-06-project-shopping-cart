d6ace3091e433eade29abedf04404b3dbbcdc42f
Marxneves <marxkhristie@gmail.com>
2020-09-01 10:20:59 -0300

    Refatorando loadStorage, emptyCart e transformando as funcoes sum e sub em updatePrice

--- script.js
+++ script.js
@@ -35,13 +35,7 @@ function storage() {
   localStorage.Lista = olList;
 }
 
-async function subCart(price) {
-  const p = document.querySelector('.total-price');
-  p.innerText = Math.round(((Number(p.innerText) - Number(price)) * 100)) / 100;
-  localStorage.price = p.innerText;
-}
-
-async function sumCart(price) {
+async function updatePrice(price) {
   const p = document.querySelector('.total-price');
   p.innerText = Math.round((Number(p.innerText) + price) * 100) / 100;
   localStorage.price = Number(p.innerText);
@@ -49,9 +43,9 @@ async function sumCart(price) {
 
 async function cartItemClickListener(event) {
   const ol = document.querySelector('ol');
-  const price = event.target.innerText.split('$')[1];
-  await subCart(price);
+  const price = Number(event.target.innerText.split('$')[1]);
   ol.removeChild(event.target);
+  await updatePrice(-price);
   storage();
 }
 
@@ -63,7 +57,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function addCart(sku) {
+function addCart(sku) {
   const url2 = `https://api.mercadolibre.com/items/${sku}`;
   fetch(url2)
     .then(response => response.json())
@@ -71,7 +65,7 @@ async function addCart(sku) {
       const li = createCartItemElement({ sku: id, name: title, salePrice: price });
       document.querySelector('.cart__items').appendChild(li);
       storage();
-      sumCart(price);
+      updatePrice(price);
     });
 }
 
@@ -95,26 +89,22 @@ function listProduct() {
       container.removeChild(loading);
     });
 }
+
 function loadStorage() {
   if (localStorage.Lista) {
     document.querySelector('.cart__items').innerHTML = localStorage.Lista;
     document.querySelectorAll('.cart__item').forEach((item) => {
       item.addEventListener('click', cartItemClickListener);
     });
-    const price = Number(localStorage.price);
-    document.querySelector('.total-price').innerText = price;
+    document.querySelector('.total-price').innerText = localStorage.price;
   }
 }
 
 function emptyCart() {
   document.querySelector('.empty-cart').addEventListener('click', () => {
-    const ol = document.querySelector('.cart__items');
-    while (ol.firstChild) {
-      ol.removeChild(ol.firstChild);
-    }
-    storage();
-    document.querySelector('.total-price').innerText = '';
-    localStorage.removeItem('price');
+    document.querySelector('.cart__items').innerHTML = '';
+    localStorage.clear();
+    document.querySelector('.total-price').innerText = '0';
   });
 }
 
