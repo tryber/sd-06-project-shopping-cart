21d9bf1d3ac4b93d1272aca2432ced8be0f11bc5
Artur Forna <fornagieri.artur@gmail.com>
2020-09-01 16:38:26 -0300

    :hurtrealbad:

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onload = function onload() {
-  fetchApiProductList();
-};
-
 function createProductImageElement(imageSource) { // Cria a imagem do produto
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,6 +13,36 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Pega id do item
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// Ao clicar no produto do carrinho, remove-o
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+// Cria a lista de items dentro do carrinho
+function createCartItemElement(sku, name, salePrice) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Pega os itens da api que serão adicionados no carrinho de compras
+const fetchApiCartProductList = (sku) => {
+  const url = `https://api.mercadolibre.com/items/${sku}`;
+  fetch(url)
+  .then(response => response.json())
+  .then((response) => {
+    const getCarItemsList = document.querySelector('.cart__items');
+    getCarItemsList.appendChild(createCartItemElement(response.id, response.title, response.price));
+  });
+};
+
 // Cria o produto
 function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
@@ -27,46 +53,12 @@ function createProductItemElement(sku, name, image) {
   section.appendChild(createProductImageElement(image));
   const buttonAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
 
-  buttonAdd.addEventListener('click', cartItemAddClickListener);
+  buttonAdd.addEventListener('click', cartItemAddClickListener = (event) => {
+    fetchApiCartProductList(getSkuFromProductItem(event.target.parentNode));
+  });
 
   section.appendChild(buttonAdd);
   return section;
-}
-
-// Pega id do item
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-// Adiciona item no carrinho ao clicar no botão add
-function cartItemAddClickListener(event) {
-  fetchApiCartProductList(getSkuFromProductItem(event.target.parentNode));
-}
-
-// Ao clicar no produto do carrinho, remove-o
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
-// Pega os itens da api que serão adicionados no carrinho de compras
-const fetchApiCartProductList = (sku) => {
-  const url = `https://api.mercadolibre.com/items/${sku}`;
-
-  fetch(url)
-  .then(response => response.json())
-  .then((response) => {
-    const getCarItemsList = document.querySelector('.cart__items');
-    getCarItemsList.appendChild(createCartItemElement(response.id, response.title, response.price));
-  });
-};
-
-// Cria a lista de items dentro do carrinho
-function createCartItemElement(sku, name, salePrice) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 // Pega os itens da api e cria a listagem de produtos
@@ -86,4 +78,8 @@ const fetchApiProductList = () => {
       getSectionItems.appendChild(createProduct);
     });
   });
+};
+
+window.onload = function onload() {
+  fetchApiProductList();
 };
