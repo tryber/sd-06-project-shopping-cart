cfe58b8c27f81cd0ef0c81454ab898bbe37b7aa6
Artur Forna <fornagieri.artur@gmail.com>
2020-09-01 08:23:41 -0300

    Task 1 - Done

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+    fetchApi();
+ };
 
-function createProductImageElement(imageSource) {
+function createProductImageElement(imageSource) { // Cria a imagem do produto
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+// Cria um 'custom html a partir de um element com a class 'className' e conteudo 'innerText'
+function createCustomElement(element, className, innerText) { 
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// Cria o produto 
+function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,6 +30,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// Pega sku
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -40,4 +45,24 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+// Query 
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computadores';
+
+// Pega os itens da api
+const fetchApi = () => {
+  fetch(url)
+  .then(response => response.json())
+  .then((response) => {
+
+    const getSectionItems = document.querySelector('.items');
+
+    response.results.forEach(product => {
+
+      const createProduct = createProductItemElement(product.id, product.title, product.thumbnail);
+      getSectionItems.appendChild(createProduct);
+    });
+
+  });
 }
