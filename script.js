1b4204b32adaa0d39812a2f8089d7fc02c175e3a
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-01 17:32:10 -0300

    Fixing loading issues

--- script.js
+++ script.js
@@ -90,12 +90,14 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 function listElements(arrResults) {
+  const secItems = document.querySelector('.items');
+  const span = document.querySelector('.loading');
+  secItems.removeChild(span);
+
   arrResults.forEach((product) => {
     const sku = product.id;
     const name = product.title;
     const image = product.thumbnail;
-    const secItems = document.querySelector('.items');
-    document.querySelector('.loading').style.display = 'none';
     secItems.appendChild(createProductItemElement({ sku, name, image }));
   });
 }
