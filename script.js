a4a371a938d3d5ed48b0c1261cb711f46217baf1
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-01 11:18:35 -0300

    Adding the third requirement Remove the item from the shopping cart by clicking on it

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// function cartItemClickListener(event) {
-
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
