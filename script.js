cea7a16bd9152bc7ac8f102c3ad43cafc520d701
rafMewtwo <ponciano.raf@gmail.com>
2020-09-01 01:17:19 -0300

    lets do this! LEEEEROYYYYYY JENKINSSS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() { 
+  fetchCurrency();
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,13 +16,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// tirar duvida do obj distruct
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
@@ -41,3 +44,43 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// mark add <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+const  apiInfo  =  {
+  api : 'https://api.mercadolibre.com/sites/MLB/search?q=$' ,
+  endpoint : 'computador'
+}
+
+const url = `${apiInfo.api}${apiInfo.endpoint}`
+
+// função fetch para pegar o obj do api do mercado livre
+const fetchCurrency = (currency) => {
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((object) => {
+      console.log(object);
+      if (object.error) {
+        throw new Error(object.error);
+      } else {
+        console.log(object.results)
+        // caminha na array object.results
+        object.results.forEach(item => {
+          // seleciona a section com id items e vai add p/ cada elemento da array o return da função createProductItemElement.
+          // o parametro item é o objeto gerado pela array object.results
+          document.querySelector('.items')
+          // tirar duvida do obj distruct
+          .appendChild(createProductItemElement(item))
+          console.log(item.title)
+        })
+      }
+    })
+    .catch((error) => handleError(error))
+}
+
+// função de erro, caso de erro
+const handleError = (errorMessage) => {
+  window.alert(errorMessage);
+}
+
