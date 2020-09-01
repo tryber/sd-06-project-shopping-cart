b36564eecdd9c5019c3c2bb386fdb2cfda4f72bc
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-01 17:51:02 -0300

    Adding the fourth requirement Load the shopping cart through LocalStorage when starting the page

--- script.js
+++ script.js
@@ -18,8 +18,15 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const localStorageShopCart = () => {
+  const shopCart = document.querySelector('.cart__items').innerHTML;
+  localStorage.shopCart = shopCart;
+  // console.log(shopCart);
+};
+
 function cartItemClickListener(event) {
   event.target.remove();
+  localStorageShopCart();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -27,6 +34,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  localStorageShopCart();
   return li;
 }
 
@@ -44,7 +52,8 @@ function idProductSelected() {
     .then(response => response.json())
     .then((object) => {
       handleCreatListCart(object);
-    });
+    })
+    .then(() => localStorageShopCart());
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -56,7 +65,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
     .addEventListener('click', idProductSelected);
-
+  localStorageShopCart();
   return section;
 }
 
@@ -73,6 +82,20 @@ const apiJSON = () => {
     .then(object => filterItemsProducts(object.results));
 };
 
+const saveLocalStorage = () => {
+  if (localStorage.shopCart) {
+    document.querySelector('.cart__items').innerHTML = localStorage.shopCart;
+  }
+};
+
+const removeCartItemClickListener = () => {
+  document.querySelectorAll('.cart__item')
+    .forEach(item => item.addEventListener('click', cartItemClickListener));
+  localStorageShopCart();
+};
+
 window.onload = function onload() {
   apiJSON();
+  saveLocalStorage();
+  removeCartItemClickListener();
 };
