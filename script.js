671d46de2e88c19cd1dcf04c605735a31f36d2e2
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-09-01 15:21:13 -0300

    Start shoopping cart

--- script.js
+++ script.js
@@ -14,30 +14,43 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+    //   const {id=sku, title = name, thumbnail = image} = data
+    //   console.log(data.results);
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+window.onload = () => {
+  const itemCart = document.querySelector('.items');
+  const urlApi = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(urlApi)
+  .then(res => res.json())
+  .then((data) => {
+    data.results.forEach((elemento) => {
+      itemCart.appendChild(createProductItemElement(elemento));
+    });
+  });
+};
