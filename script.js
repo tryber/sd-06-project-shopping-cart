d187d1dbe90ee21e487ee515523723ffb6e3e671
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-01 17:25:55 -0300

    solicionando item 1 e solucionando Code Climate

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
