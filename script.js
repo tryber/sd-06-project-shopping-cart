949b4c174b59825ecbd055de78dc5d35ec3ef022
Elisa <elisatcamillo@gmail.com>
2020-09-01 01:09:01 -0300

    first req

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  createList();
+};
+
+async function createList() {
+  const items = document.getElementsByClassName('items')[0];
+  // const query = document.getElementById('query_input').value;
+  // acho que talvez o teste não peça por um retorno de qqr pesquisa, só de "computador"
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  await fetch(url)
+    .then(r => r.json())
+    .then(r => r.results.map(item => createProductItemElement(item)))
+    .then(itemList => itemList.forEach((item) => {
+      items.appendChild(item);
+    }));
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,3 +56,34 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+
+
+
