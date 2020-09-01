3a7a92762ec1be8ee30598bc835060ef7c5ad949
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-01 13:46:51 -0300

    requisito 2

--- script.js
+++ script.js
@@ -26,21 +26,36 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqu
-// }
+function cartItemClickListener(event) {
+  const buttonItem = event.querySelector('.item__add');
+  const itemID = getSkuFromProductItem(event);
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+  buttonItem.addEventListener('click', function () { addToCart(itemID) });
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  
+  return li;
+}
+
+function addToCart(productID) {
+  const cart = document.querySelector('.cart__items');
+
+  fetch(`https://api.mercadolibre.com/items/${productID}`)
+    .then(response => response.json())
+    .then(response => {
+      const productToCart = createCartItemElement(response);
+      cart.appendChild(productToCart);
+    });
+}
 
 function fetchComputers() {
   const section = document.querySelector('.items');
@@ -50,6 +65,7 @@ function fetchComputers() {
     .then(response => response.results)
     .then(arrayOfComputers => arrayOfComputers.forEach((computer) => {
       const product = createProductItemElement(computer);
+      cartItemClickListener(product);
       section.appendChild(product);
     }));
 }
