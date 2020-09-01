10e4b723fffde2dfbd267ac4364c78628d0e5612
Artur Forna <fornagieri.artur@gmail.com>
2020-09-01 15:12:00 -0300

    :suspect:

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function onload() {
-    fetchApi();
- };
+  fetchApiProductList();
+};
 
 function createProductImageElement(imageSource) { // Cria a imagem do produto
   const img = document.createElement('img');
@@ -10,14 +10,14 @@ function createProductImageElement(imageSource) { // Cria a imagem do produto
 }
 
 // Cria um 'custom html a partir de um element com a class 'className' e conteudo 'innerText'
-function createCustomElement(element, className, innerText) { 
+function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-// Cria o produto 
+// Cria o produto
 function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -25,21 +25,43 @@ function createProductItemElement(sku, name, image) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const buttonAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
 
+  buttonAdd.addEventListener('click', cartItemAddClickListener);
+
+  section.appendChild(buttonAdd);
   return section;
 }
 
-// Pega sku
+// Pega id do item
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+// Adiciona item no carrinho ao clicar no botão add
+function cartItemAddClickListener(event) {
+  fetchApiCartProductList(getSkuFromProductItem(event.target.parentNode));
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+// Ao clicar no produto do carrinho, remove-o
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+// Pega os itens da api que serão adicionados no carrinho de compras
+const fetchApiCartProductList = (sku) => {
+  const url = `https://api.mercadolibre.com/items/${sku}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then((response) => {
+    const getCarItemsList = document.querySelector('.cart__items');
+    getCarItemsList.appendChild(createCartItemElement(response.id, response.title, response.price));
+  });
+};
+
+// Cria a lista de items dentro do carrinho
+function createCartItemElement(sku, name, salePrice) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -47,22 +69,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-// Query 
-const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computadores';
+// Pega os itens da api e cria a listagem de produtos
+const fetchApiProductList = () => {
+  // Query
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computadores';
 
-// Pega os itens da api
-const fetchApi = () => {
   fetch(url)
   .then(response => response.json())
   .then((response) => {
-
+    // Seleciona section items
     const getSectionItems = document.querySelector('.items');
 
-    response.results.forEach(product => {
-
+    // Cria o produto e faz o append a section items
+    response.results.forEach((product) => {
       const createProduct = createProductItemElement(product.id, product.title, product.thumbnail);
       getSectionItems.appendChild(createProduct);
     });
-
   });
-}
+};
