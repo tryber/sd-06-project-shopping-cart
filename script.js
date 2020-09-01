dc78142e90856259550396f965b17ec90dd6e42b
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-31 22:54:18 -0300

    RE-starting - 01, 02, 03 and 06th requirements done

--- script.js
+++ script.js
@@ -17,66 +17,13 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// 04 Carregue o carrinho de compras através do
-// LocalStorage ao iniciar a página
-// removeFromStorage remove o elemento recebido
-// e se o item no array no localStorage for ===
-// ao id do elemento target do evento, ele
-function removeFromStorage(element) {
-  const textElement = element.innerText.split('|');
-  const itemId = textElement[0].split(':')[1];
-  const itemsProd = localStorage.getItem('cart').split(',');
-  for (let i = 0; i < itemsProd.length; i += 3) {
-    if (itemsProd[i].trim() === itemId.trim()) {
-      itemsProd.splice(itemsProd.indexOf(itemsProd[i]), 3);
-      localStorage.setItem('cart', itemsProd);
-    }
-  }
-}
 
-// 04 Carregue o carrinho de compras através do
-// LocalStorage ao iniciar a página
-// nessa funcao, caso o item seja removido do carrinho
-// de compras ele tb  sera removido do localStorage
-function checkStorage(element) {
-  if (localStorage.cart.split(',').length === 3) {
-    localStorage.clear();
-  } else {
-    removeFromStorage(element);
-  }
-}
 
 // 03 Remova o item do carrinho de compras ao clicar nele
 function cartItemClickListener(event) {
   const cartList = document.querySelector('.cart__items');
   const element = event.target;
-  checkStorage(element);
   cartList.removeChild(element);
-}
-
-// 04 Carregue o carrinho de compras através do
-// LocalStorage ao iniciar a página
-// nessa funcao pegamos o que ja existe no localStorage
-// para ser renderizado na tela quando acontecer o window.onload
-// primeiro passamos por um if, para ver se existe
-// alguma informacao salva no localStorage
-// se existir, dividimos esse arr nas ','
-// e fazemos um for para que possamos ler cada
-// parte do arr dentro das variaveis na string
-// adicionando esses itens no innertext do li
-// criado na funcao dentro da lista do carrinho de compras
-// e dando um appendChild para inseri-los na lista
-function renderLocallySavedCart() {
-  if (localStorage.cart !== '' && localStorage.length !== 0) {
-    const items = localStorage.getItem('cart').split(',');
-    for (let i = 0; i < items.length; i += 3) {
-      const li = document.createElement('li');
-      li.className = 'cart__item';
-      li.innerText = `SKU: ${items[i].trim()} | NAME: ${items[i + 1].trim()} | PRICE: $${items[i + 2]}`;
-      li.addEventListener('click', cartItemClickListener);
-      document.querySelector('.cart__items').appendChild(li);
-    }
-  }
 }
 
 // 02 Adicionando o produto ao carrinho de compras
@@ -92,17 +39,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
-// 04 Carregue o carrinho de compras através do
-// LocalStorage ao iniciar a página
-// a funcao saveCartLocally recebe como parametros o
-// id, nome e preco do produto e caso o localStorage
-// esteja vazio, ele salva os itens atuais do carrinho de compras atual
-function saveCartLocally({ id, price, title }) {
-  if (localStorage.length === 0 || localStorage.cart === '') {
-    localStorage.setItem('cart', `${id}, ${title}, ${price}`);
-  }
 }
 
 // 01 Listagem de produtos. criar uma listagem de produtos
@@ -166,6 +102,7 @@ function fetchProducts() {
     });
 }
 
+
 // 06 Botão para limpar carrinho de compras
 // nesta funcao clearCartButton primeiro limpamos o localStorage
 // depois usamos o while para que ENQUANTO a lista
@@ -174,7 +111,6 @@ function fetchProducts() {
 function clearCartButton(event) {
   localStorage.clear();
   const cartItems = document.querySelector('.cart__items');
-  localStorage.clear();
   while (cartItems.childNodes.length > 0) {
     cartItems.removeChild(cartItems.childNodes[0]);
   }
@@ -182,6 +118,5 @@ function clearCartButton(event) {
 
 window.onload = function onload() {
   fetchProducts();
-  renderLocallySavedCart();
   document.querySelector('.empty-cart').addEventListener('click', clearCartButton);
 };
