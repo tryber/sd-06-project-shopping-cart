4d6db76235adedebed4de427ce5353cff2be8268
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-08-31 21:32:41 -0300

    Requisito 5.

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-function saveCurrentList() {
-  const cartList = document.querySelector('.cart__items');
-  const listSaver = cartList.innerHTML;
-  localStorage.setItem('SavedList', listSaver);
-}
-
-function cartItemClickListener(event, cartList) {
-  cartList.removeChild(event);
-  saveCurrentList();
-}
-
-window.onload = function onload() {
-  const cartList = document.querySelector('.cart__items');
-  cartList.addEventListener('click', () => {
-    cartItemClickListener(event.target, cartList);
-  });
-
-  function cartItensHandler() {
-    const savedList = localStorage.getItem('SavedList');
-    if (savedList) {
-      cartList.innerHTML = savedList;
-    }
-  }
-
-  cartItensHandler();
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -75,6 +48,37 @@ function createItem(listItem) {
   return newItem;
 }
 
+function fetchItem(itemID) {
+  const apiItem = `https://api.mercadolibre.com/items/${itemID}`;
+  return fetch(apiItem).then(response => response.json());
+}
+
+async function totalValueUpdater() {
+  const totalPriceDisplay = document.querySelector('.total-price');
+  const cartList = document.querySelector('.cart__items');
+  let totalPrice = 0;
+
+  for (let x = cartList.firstChild; x; x = x.nextElementSibling) {
+    console.log(x);
+    const nextID = x.innerText.substr(5, 13);
+    const itemPrice = await fetchItem(nextID).then(item => item.price);
+    totalPrice += itemPrice;
+  }
+  totalPriceDisplay.innerText = totalPrice;
+}
+
+function saveCurrentList() {
+  const cartList = document.querySelector('.cart__items');
+  const listSaver = cartList.innerHTML;
+  totalValueUpdater();
+  localStorage.setItem('SavedList', listSaver);
+}
+
+function cartItemClickListener(event, cartList) {
+  cartList.removeChild(event);
+  saveCurrentList();
+}
+
 function itemCreator(item, createFunction, listClassName) {
   const fetchedItemData = createItem(item);
   const newItem = createFunction(fetchedItemData);
@@ -89,27 +93,41 @@ function addItemToCart(eventElement) {
   if (eventElement.className === 'item__add') {
     const selectedItem = eventElement.parentNode;
     const itemID = getSkuFromProductItem(selectedItem);
-    const apiItem = `https://api.mercadolibre.com/items/${itemID}`;
-    fetch(apiItem).then(response => response.json())
+    fetchItem(itemID)
       .then((data) => {
         itemCreator(data, createCartItemElement, '.cart__items');
       });
   }
 }
 
-async function createItemList() {
-  await fetch(apiList).then(response => response.json())
+function createItemList() {
+  fetch(apiList).then(response => response.json())
     .then(data => data.results
       .forEach((computer) => {
         itemCreator(computer, createProductItemElement, '.items');
-      }))
-    .then(() => {
-      const addButton = document.querySelector('.items');
-      addButton.addEventListener('click', () => {
-        const button = event.target;
-        addItemToCart(button);
-      });
-    });
+      }));
 }
 
 createItemList();
+
+window.onload = function onload() {
+  const cartList = document.querySelector('.cart__items');
+  cartList.addEventListener('click', () => {
+    cartItemClickListener(event.target, cartList);
+  });
+
+  function cartItensHandler() {
+    const savedList = localStorage.getItem('SavedList');
+    if (savedList) {
+      cartList.innerHTML = savedList;
+    }
+  }
+
+  cartItensHandler();
+
+  const addButton = document.querySelector('.items');
+  addButton.addEventListener('click', () => {
+    const button = event.target;
+    addItemToCart(button);
+  });
+};
