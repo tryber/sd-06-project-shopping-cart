dc79bda3ddd2fa1c9b80d0910951e4e5991ae079
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-01 14:39:37 -0300

    02- refatorando o fetchUrl

--- script.js
+++ script.js
@@ -7,14 +7,47 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+
+//   // const olCarrinho = document.querySelector('.cart_items');
+//   // olCarrinho.appendChild(li);
+//   return li;
+// }
+
+// function cartItemClickListener(event) {
+
+//   const idCardTarget = event.target.previousSibling.previousSibling.previousSibling.innerText;
+//   const url = `https://api.mercadolibre.com/items/${idCardTarget}`;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(e => console.log(e));
+//   .then(cpu => {
+//     const finale = {
+//       sku: cpu.id,
+//       name: cpu.title,
+//       salePrice: cpu.price,
+//     };
+//     console.log(finale);
+//     createCartItemElement(finale);
+//   });
+// }
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  // e.addEventListener('click', cartItemClickListener);
   return e;
 }
-
-function createProductItemElement({ sku, name, image }) {
+// destructuring nos computadores q vem do fetch
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
   section.appendChild(createCustomElement('span', 'item__sku', sku));
@@ -22,43 +55,26 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  const productSection = document.querySelector('.items');
-  productSection.appendChild(section);
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-// Função para pegar resposta da api
 const fetchUrl = () => {
   const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   fetch(url)
-  .then(response => response.json())
-  // .then(object => console.log(object.results));
-  .then((data) => {
-    data.results.forEach((cpu) => {
-      const finale = {
-        sku: cpu.id,
-        name: cpu.title,
-        image: cpu.thumbnail,
-      };
-      createProductItemElement(finale);
+    .then(response => response.json())
+    .then((data) => {
+      data.results.forEach((cpu) => {
+        // const finale = {      // obs: deu certo também!!
+        //   sku: cpu.id,
+        //   name: cpu.title,
+        //   image: cpu.thumbnail,
+        // };
+        // createProductItemElement(finale);
+        const productSection = document.querySelector('.items');
+        const computadores = createProductItemElement(cpu);
+        productSection.appendChild(computadores);
+      });
     });
-  });
 };
 
 window.onload = function onload() {
