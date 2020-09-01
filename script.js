27d992eb27619009286c30f7202968c9819a224e
Alana Monteiro <lanaah.15@gmail.com>
2020-09-01 13:21:11 -0300

    fetch realizado, tentando criar primeira funcao

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 window.onload = function onload() { };
+const itemsArray = await itemsList('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+.then((arr) => {
+  arr.map()
+}
+);
 
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+
+async function itemsList (url)  {
+  if (url === 'https://api.mercadolibre.com/sites/MLB/search?q=$computador') {
+   return fetch(url)
+   .then(r => r.json())
+   .then(r => r.results);
+   } else {
+     throw new Error(error);
+   }
 }
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
+// function createProductImageElement(imageSource) {
+//   const img = document.createElement('img');
+//   img.className = 'item__image';
+//   img.src = imageSource;
+//   return img;
+//   //"thumbnail"
+// }
+
+// function createCustomElement(element, className, innerText) {
+//   const e = document.createElement(element);
+//   e.className = className;
+//   e.innerText = innerText;
+//   return e;
+// }
+
+itemsArray.forEach(element => {
+  let sku = element.id;
+  let name = element.title;
+  let image = element.thumbnail;
+  createProductItemElement(sku, name, image);
+});
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -26,24 +50,19 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui 
-  // Você deve criar uma listagem de produtos que devem ser consultados através da API do Mercado Livre.
 
-  // Você deve utilizar o endpoint:
+// function cartItemClickListener(event) {
+//   document.getElementById
+// }
 
-  // "https://api.mercadolibre.com/sites/MLB/search?q=$QUERY"
-  // onde $QUERY deve ser o valor da sua busca. Para este trabalho, a busca deve ser o termo computador.
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
