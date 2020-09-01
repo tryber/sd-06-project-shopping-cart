492f4c0f8d29f5b3303daf9f89e919ca09fb809b
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-01 11:13:32 -0300

    Adding the second requirement Add the product to the shopping cart

--- script.js
+++ script.js
@@ -14,6 +14,39 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// function cartItemClickListener(event) {
+
+// }
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const handleCreatListCart = (objectToShopCart) => {
+  const arrayProducts = document.querySelector('.cart__items');
+  arrayProducts.appendChild(createCartItemElement(objectToShopCart));
+};
+
+function idProductSelected() {
+  const eventClick = event.target.parentElement;
+  const idProduct = getSkuFromProductItem(eventClick);
+  const endpoint = `https://api.mercadolibre.com/items/${idProduct}`;
+
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((object) => {
+      handleCreatListCart(object);
+    });
+}
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,7 +54,8 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', idProductSelected);
 
   return section;
 }
@@ -38,22 +72,6 @@ const apiJSON = () => {
     .then(response => response.json())
     .then(object => filterItemsProducts(object.results));
 };
-
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
 
 window.onload = function onload() {
   apiJSON();
