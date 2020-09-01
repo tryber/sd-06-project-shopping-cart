9009d192b7a279ebc9e86ab0cb540c1ec6b4b5c4
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-01 18:24:54 -0300

    7° requisito funcional code climat 2

--- scriptAsync.js
+++
@@ -1,6 +0,0 @@
-window.onload = function onload() {
-  const listPurchase = document.querySelector('.cart__items');
-  listPurchase.addEventListener('change', () => {
-  listPurchase.children;
-  });
-}
