28b947ce1c32a2e990f04a9713338c30584b29ac
rafaelgelatti <rafael.gelatti@gmail.com>
2020-09-02 13:34:35 -0300

    testing localStorage

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let totalPrice = 0;
-
 const getTotalPrice = (itemAdd, itemRemove) => {
   const totalPriceContainer = document.getElementById('price');
   if (itemAdd) { totalPrice += itemAdd; }
@@ -12,9 +11,12 @@ function cartItemClickListener(event) {
   const localKeys = Object.keys(localStorage);
   const localValues = Object.values(localStorage);
   localKeys.forEach((key, index) => {
-    if (event.target.innerText.includes(key)) {
+    // verifies if the item clicked has the id stored, so removes the correct key
+    if (event.target.innerText.includes(JSON.parse(localValues[index]).id)) {
       // removing item from LocalStorage
-      getTotalPrice(0, localValues[index]);
+      // learned how to search a way to store and retrieve objects in localStorage from prof Ícaro
+      // and here: https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+      getTotalPrice(0, JSON.parse(localValues[index]).price);
       localStorage.removeItem(key);
     }
   });
@@ -59,6 +61,10 @@ const clearCart = (items, button) => {
   });
 };
 
+function addToLocalStorage(product, index) {
+  localStorage.setItem(index, JSON.stringify(product));
+}
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -72,14 +78,26 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
     .then((product) => {
       const item = createCartItemElement(product);
       const shoppingCart = document.querySelector('.cart__items');
-      // if (!shoppingCart.innerText.includes(product.id)) {
-      // adding item to cart
-      shoppingCart.appendChild(item);
-      // adding item to localStorage
-      localStorage.setItem(product.id, product.price);
-      // totalPrice increase
-      getTotalPrice(product.price, 0);
-      // }
+      if (!shoppingCart.innerText.includes(product.id)) {
+        // adding item to cart
+        shoppingCart.appendChild(item);
+        // adding item to localStorage
+        // learned how to search a way to store and retrieve objects in localStorage from prof Ícaro
+        // and here: https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+        if (!localStorage.length) {
+          addToLocalStorage(product, 0);
+        } else {
+          // makes indexes always higher so i can keep the order when i load localStorage
+          let highestIndex = 0;
+          Object.keys(localStorage).forEach((key) => {
+            if (parseInt(key) > highestIndex) { highestIndex = parseInt(key); }
+          });
+          addToLocalStorage(product, highestIndex + 1);
+        }
+
+        // totalPrice increase
+        getTotalPrice(product.price, 0);
+      }
       const clearButton = document.getElementById('empty-cart');
       clearCart(shoppingCart, clearButton);
     });
@@ -113,17 +131,52 @@ function getSkuFromProductItem(item) {
 
 window.onload = function onload() {
   fetchAPI('computador');
-  const shoppingCart = document.querySelector('.cart__items');
-  const keys = Object.keys(localStorage);
-  keys.forEach((key) => {
-    fetch(`https://api.mercadolibre.com/items/${key}`)
-    .then(r => r.json())
-    .then((product) => {
-      const item = createCartItemElement(product);
-      shoppingCart.appendChild(item);
-      getTotalPrice(product.price);
-    });
+  // const shoppingCart = document.querySelector('.cart__items');
+  // const keys = Object.keys(localStorage);
+  // keys.forEach((key) => {
+  //   fetch(`https://api.mercadolibre.com/items/${JSON.parse(localStorage[key]).id}`)
+  //   .then(r => r.json())
+  //   .then((product) => {
+  //     const item = createCartItemElement(product);
+  //     shoppingCart.appendChild(item);
+  //     getTotalPrice(product.price);
+  //   });
+  // });
+
+  const cart = document.querySelector('.cart__items');
+  Object.keys(localStorage).sort().forEach((key, index) => {
+    for (let i = 0; i < localStorage.length; i += 1) {
+      if (i === index) {
+        const product = JSON.parse(localStorage[index]);
+        const item = createCartItemElement(product);
+        cart.appendChild(item);
+        getTotalPrice(product.price);
+        // console.log(JSON.parse(localStorage[index]).id)
+      }
+    }
+    // JSON.parse(localStorage[index]).id
   });
+
+  // const newArr = Object.keys(localStorage);
+  // const values = Object.values(localStorage);
+  // const newArrOfValues = [];
+  // newArr.forEach((item) => {
+  //   for (let count = 0; count < newArr.length; count += 1) {
+  //     if (localStorage.key(count) === item) {
+  //       newArrOfValues.push(values[count]);
+  //     }
+  //   }
+  // });
+  // localStorage.clear();
+  // newArrOfValues.forEach((value, index) => {
+  //   localStorage.setItem(index, value);
+  // });
+
+  // for (let index = 0; index < localStorage.length; index += 1) {
+  //   const item = createCartItemElement(product);
+  //   shoppingCart.appendChild(item);
+  // }
+
   const cartItems = document.querySelector('#cart-items');
   const clearButton = document.getElementById('empty-cart');
   clearCart(cartItems, clearButton);
