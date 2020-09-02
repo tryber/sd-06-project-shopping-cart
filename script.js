2c268da425a149c428ab837f72762e28a92f1d0d
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-02 01:30:50 -0300

    Adicionados comentários para detalhar as funções implementadas

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
+  // Função nativa responsável por renderizar as imagens da API na tela
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
+  // Função nativa responsável por renderizar os textos dos produtos captados da API na tela
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
+  // Função nativa responsável por renderizar os números de ID retirados da API na tela
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+  // Requisito 3 - Função responsável por remover um item do
+  // carrinho de compras ao clicarmos em cima dele.
+function cartItemClickListener(event) {
+  const total = document.querySelector('.total-price');
+  const priceToRemove = parseFloat((event.target.innerText).split('$')[1]);
+  const totalPrice = parseFloat(total.innerHTML);
+  const sub = totalPrice - priceToRemove;
+  total.innerText = sub;
+  event.target.remove();
+}
+  // Requisito 5 - Função responsável por somar todos os
+  // preços dos produtos adicionados no carrinho de compras.
+async function sumItems(li) {
+  const total = document.querySelector('.total-price');
+  const itemPrice = parseFloat(li.innerText.split('$')[1]);
+  const totalPrice = parseFloat(total.innerHTML);
+  const sum = itemPrice + totalPrice;
+  total.innerText = sum;
+}
+  // Função nativa responsável por criar as descrições dos
+  // itens quando adicionados ao carrinho de compras.
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  sumItems(li);
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+  // Função nativa que cria os produtos com seus nomes, ids e imagens.
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,37 +55,11 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
 }
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-// Requisito 3
-function cartItemClickListener(event) {
-  const total = document.querySelector('.total-price');
-  const priceToRemove = parseFloat((event.target.innerText).split('$')[1]);
-  const totalPrice = parseFloat(total.innerHTML);
-  const sub = totalPrice - priceToRemove;
-  total.innerText = sub;
-  event.target.remove();
-}
-// Requisito 5
-async function sumItems(li) {
-  const total = document.querySelector('.total-price');
-  const itemPrice = parseFloat(li.innerText.split('$')[1]);
-  const totalPrice = parseFloat(total.innerHTML);
-  const sum = itemPrice + totalPrice;
-  total.innerText = sum;
-}
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  sumItems(li);
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+  // Capta o endereço da API com a query computador solicitada no requisito 1
 const urlSearch = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
-
-// Fetching items
+  // Função responsável por fazer o fetch da API do MercadoLivre e resgatar os elementos
+  // solicitados e enviá-los para a função createCartItemElement (que cria todos os produtos)
+  // no carrinho de compras.
 const fetchItem = (firstChild) => {
   const cartSection = document.querySelector('.cart');
   const spanLoading = document.createElement('span');
@@ -68,15 +78,19 @@ const fetchItem = (firstChild) => {
       cartSection.removeChild(spanLoading);
     });
 };
-
-// Requisito 2
+  // Requisito 2 - Função responsável por adicionar os produtos
+  // ao carrinho de compras quando clicamos no botão 'adicionar ao carrinho'.
 const getItem = () => {
   const target = event.target;
   const parentTarget = target.parentElement;
   const firstChild = parentTarget.firstChild.innerText;
   fetchItem(firstChild);
 };
-// Requisito 1
+  // Requisito 1 e 7 - Função responsável por fazer o fetch
+  // da API do MercadoLivre e resgatar os elementos
+  // solicitados e enviá-los para a função createProductItemElement
+  // (que cria todos os elementos). Além disso cria o elemento 'loading'
+  // para simular carregamento enquanto a requisição fetch é feita!
 const fetchUrl = () => {
   const cartSection = document.querySelector('.cart');
   const spanLoading = document.createElement('span');
@@ -98,7 +112,7 @@ const fetchUrl = () => {
       cartSection.removeChild(spanLoading);
     });
 };
-// Requisito 6
+  // Requisito 6 - Botão que limpa todos os itens do carrinho de compras.
 const emptyList = () => {
   document.querySelector('.empty-cart').addEventListener('click', () => {
     const cart = document.querySelector('.cart__items');
@@ -112,3 +126,5 @@ window.onload = function onload() {
   fetchUrl();
   emptyList();
 };
+  // Requisito 4
+  // FALTA EXECUTAR!
