0c324074653f29102a05238b23163f4a57c47afd
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-03 18:14:01 -0300

    segundo requisito 1.1 cc

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
 }
 
@@ -37,43 +37,46 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-function productId (sku){
-    fetch(`https://api.mercadolibre.com/items/${sku}`)
-    .then(response => response.json())
-    .then(({id, title, price}) => {
+function productId(sku) {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+  .then(response => response.json())
+  .then(({id, title, price}) => {
       // retornar essas propriedades
       const addedProductOnCart = createCartItemElement({
         sku: id,
         name: title,
-        salePrice:price
-      })
-    // onde serão appendados na seção do carrinho de compras apos ser chamado no fetch 1 após criação do ol no index.html
-    document.querySelector('.cart__items').appendChild(addedProductOnCart);
-    })
+        salePrice: price,
+      });
+    // onde serão appendados na 
+    //seção do carrinho de 
+    //compras apos ser chamado
+    // no fetch 1 após criação 
+    //do ol no index.html
+        document.querySelector('.cart__items').appendChild(addedProductOnCart);
+    });
   }
-  
+
 function fetchMercadoLivre() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then(response => response.json())
-    .then((object) => { 
+    .then((object) => {
       object.results.forEach(({ id, title, thumbnail }) => {
         // results done
-        const product =  createProductItemElement({
+        const product = createProductItemElement({
           sku: id,
           name: title,
-          image: thumbnail 
+          image: thumbnail
         });
-         product.addEventListener('click', (event) =>{
-         if(event.target.className === 'item__add'){
-          productId(getSkuFromProductItem(event.target.parentElement));
-         }
-       })
+          product.addEventListener('click', (event) =>{
+            if(event.target.className === 'item__add'){
+              productId(getSkuFromProductItem(event.target.parentElement));
+          }
+        })
         // retrieve images done
         document.querySelector('.items').appendChild(product);
         // adição das imagens
       });
     });
-    
 }
 window.onload = function onload() {
   fetchMercadoLivre();
