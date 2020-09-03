86688137c08b4fcfda06fcf8eb645a8f0e461daa
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-03 18:30:54 -0300

    segundo requisito cc 1.5

--- script.js
+++ script.js
@@ -41,40 +41,39 @@ function productId(sku) {
   fetch(`https://api.mercadolibre.com/items/${sku}`)
   .then(response => response.json())
   .then(({ id, title, price }) => {
-      // retornar essas propriedades
+    // retornar essas propriedades
     const addedProductOnCart = createCartItemElement({
-        sku: id,
-        name: title,
-        salePrice: price,
-      });
+      sku: id,
+      name: title,
+      salePrice: price,
+    });
     // onde serão appendados na seção do carrinho de
     // compras apos ser chamado no fetch 1 após criação
     // do ol no index.html
-    document.querySelector('.cart__items').appendChild(addedProductOnCart);
-    });
+    (document.querySelector('.cart__items').appendChildaddedProductOnCart);
+  });
 }
-
 function fetchMercadoLivre() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then((object) => {
-      object.results.forEach(({ id, title, thumbnail }) => {
-        // results done
-        const product = createProductItemElement({
+  .then(response => response.json())
+  .then((object) => {
+    object.results.forEach(({ id, title, thumbnail }) => {
+      // results done
+      const product = createProductItemElement({
         sku: id,
         name: title,
         image: thumbnail,
       });
-          product.addEventListener('click', (event) => {
-            if (event.target.className === 'item__add') {
-              productId(getSkuFromProductItem(event.target.parentElement));
-          }
-        });
-        // retrieve images done
-        document.querySelector('.items').appendChild(product);
-        // adição das imagens
+      product.addEventListener('click', (event) => {
+        if (event.target.className === 'item__add') {
+          productId(getSkuFromProductItem(event.target.parentElement));
+        }
       });
+      // retrieve images done
+      document.querySelector('.items').appendChild(product);
+      // adição das imagens
     });
+  });
 }
 window.onload = function onload() {
   fetchMercadoLivre();
