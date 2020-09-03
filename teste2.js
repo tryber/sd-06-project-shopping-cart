ff769d71d2c5e889a42375b0783ee967be97cebb
Paulo <paulocheloni@gmail.com>
2020-09-03 16:42:27 -0300

    fixing lint

--- teste2.js
+++
@@ -1,7 +0,0 @@
-function vowels (str) {
-  const newStr = str.split('');
-  const result = newStr.filter((curr) => curr === 'a' || curr === 'e' || curr === 'i' || curr === 'o ' || curr ==='u');
-  return result.length
-}
-
-console.log(vowels('paulo'));
