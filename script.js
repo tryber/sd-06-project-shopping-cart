49a9c77c18d7057b0009fb268e4617cd9977aea4
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-09-04 18:49:38 -0300

    tarefa LocalStorage
    tarefa LocalStorage

--- script.js
+++ script.js
@@ -4,11 +4,66 @@ const apiURL = {
   search: 'computador',
   endPointItem: 'items/',
 };
-
-function cartItemClickListener(event) {
-  event.target.remove();
+// Mostra o valor total dos produtos no carrinho de compras.
+function showTotalPrice(sum) {
+  const total = document.querySelector('.total-price');
+  total.innerHTML = sum;
 }
-
+// Calcula de forma assíncrona o preço total dos produtos adicionados ao carrinho.
+async function getTotalPrice() {
+  let sum = 0;
+  const getCartItems = await JSON.parse(localStorage.getItem('cartML'));
+  if (getCartItems) {
+    for (let index = 0; index < getCartItems.length; index += 1) {
+      sum += getCartItems[index].price;
+    }
+  }
+  showTotalPrice(sum);
+}
+// Salva os itens do carrinho no LocalStorage.
+function saveToLocalStorage(id, title, price) {
+  if (typeof Storage !== 'undefined') {
+    let arrayOfItems = [];
+    const getCartItems = JSON.parse(localStorage.getItem('cartML'));
+    arrayOfItems = (getCartItems === null ? [] : getCartItems);
+    arrayOfItems.push({ id, title, price });
+    localStorage.setItem('cartML', JSON.stringify(arrayOfItems));
+    getTotalPrice();
+  }
+}
+// Remove um item específico do LocalStorage.
+function removeItemFromLocalStorage(sku) {
+  const arrayOfItems = JSON.parse(localStorage.getItem('cartML'));
+  for (let index = 0; index < arrayOfItems.length; index += 1) {
+    if (arrayOfItems[index].id === sku) {
+      arrayOfItems.splice(index, 1);
+      break;
+    }
+  }
+  localStorage.setItem('cartML', JSON.stringify(arrayOfItems));
+  getTotalPrice();
+}
+// Remove um item do carrinho de compras.
+function cartItemClickListener(event) {
+  const item = event.target;
+  item.remove();
+  console.log('target:' + event.target.innerHTML.slice(5, 17));
+  removeItemFromLocalStorage(event.target.innerHTML.slice(5, 17));
+  removeItemFromLocalStorage(event.target.innerHTML.slice(5, 18));
+}
+// Pega informação do LocalStorage.
+function getFromLocalStorage() {
+  if (Storage) {
+    const getProductCartItems = JSON.parse(localStorage.getItem('cartML'));
+    var arrayOfItems = (getProductCartItems === null ? [] : getProductCartItems);
+    arrayOfItems.forEach((element) => {
+      const itemProduct = createCartItemElement(element.id, element.title, element.price);
+      createProductItemElement(element.id, element.title, element.price);
+    });
+  }
+  getTotalPrice();
+}
+// Cria um elemento do carrinho de compras.
 function createCartItemElement(sku, name, salePrice) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -19,36 +74,37 @@ function createCartItemElement(sku, name, salePrice) {
   console.log(cartOl.appendChild(li));
   return li;
 }
-
+// Da um fetch no item pelo ID e chama as funções: createCartItemElement e saveToLocalStorage.
 async function fetchItemsID(itemID) {
   const itemURL = `${apiURL.url}${apiURL.endPointItem}${itemID}`;
   await fetch(itemURL)
     .then(response => response.json())
     .then((resultJSON) => {
       createCartItemElement(resultJSON.id, resultJSON.title, resultJSON.price);
+      saveToLocalStorage(resultJSON.id, resultJSON.title, resultJSON.price);
     })
     .catch(error => window.alert(error));
 }
-
+// Cria um elemento customizado no HTML.
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-
+// Cria o elemento imagem no HTML.
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
-
+// Cria o elemento HTML no carrinho.
 function renderCartItem(event) {
   const itemID = event.target.parentNode.children[0].innerText;
   fetchItemsID(itemID);
 }
-
+// Cria os elementos no HTML para a sessão principal onde se mostra os Items.
 function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -60,7 +116,7 @@ function createProductItemElement(sku, name, image) {
   section.appendChild(button);
   return section;
 }
-
+// Pega o array de produtos vinda da função fetchItems() e desenha um por um com a função: createProductItemElement.
 function listReturned(arrOfProducts) {
   arrOfProducts.forEach((element) => {
     const secItems = document.querySelector('.items');
@@ -68,7 +124,7 @@ function listReturned(arrOfProducts) {
       .appendChild(createProductItemElement(element.id, element.title, element.thumbnail));
   });
 }
-
+// Faz o fetch na API do ML e usa a função listReturned() para transformar o array de produtos retornados.
 function fetchItems() {
   const productURL = `${apiURL.url}${apiURL.endPointProduct}${apiURL.search}`;
   fetch(productURL)
@@ -79,7 +135,7 @@ function fetchItems() {
     })
     .catch(error => window.alert(error));
 }
-
+// Pega o SKU(ou ID) no HTML do item e o retorna.
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -87,6 +143,7 @@ function getSkuFromProductItem(item) {
 window.onload = function onload() {
   const loading = document.querySelector('.loading');
   fetchItems();
+  getFromLocalStorage();
   setTimeout(() => {
     loading.remove();
   }, 500);
