caf0d60e233c30199a58ee1f49149ba8a282dd3e
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-04 12:52:30 -0300

    corrigindo o tamanho das linhas

--- script.js
+++ script.js
@@ -49,12 +49,14 @@ Você deve utilizar o endpoint:
 onde $QUERY deve ser o valor da sua busca. Para este trabalho, a busca deve ser o termo computador.
 O retorno desse endpoint será algo no formato json. Por exemplo, se for pesquisado "computador":
 A lista de produtos que devem ser exibidos é o array results no JSON acima.
-Você deve utilizar a função createProductItemElement(product) para criar os componentes HTML referentes a um produto.
-Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
+Você deve utilizar a função createProductItemElement(product) para criar os
+componentes HTML referentes a um produto.
+Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento 
+<section class="items">.
 Obs: as variáveis sku, no código fornecido, se referem aos campos id retornados pela API.
 */
-
-const listaDeProdutos = async () => { // funcao que pega a lista de produtos da api e printa na página
+// funcao que pega a lista de produtos da api e printa na página
+const listaDeProdutos = async () => { 
   await fetch(url + produto)
   .then(resposta => resposta.json())
   .then((resultado) => {
