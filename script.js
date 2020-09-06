53e9bd4aa0516cac250a291bc0227ab3064f03b3
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-09-06 09:44:33 -0300

    teste

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+let query = 'computador';
 const apiInfo = {
 
-  api: 'https://developers.mercadolivre.com.br/',
-  endpoint: 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY'
-  
+  api: 'https://api.mercadolibre.com/',
+  endpoint: 'sites/MLB/search?q='
+
 }
 
- const url = `${apiInfo.api} ${apiInfo.endpoint}`
+const url = `${apiInfo.api}${apiInfo.endpoint}${query}`
+
+const fecthItems = (url) => {
+  console.log(url)
+  fetch(url)
+    .then((response) => response.json())
+
+    .then((object) => {
+      console.log(object);
+      if (object.error) {
+
+        console.log(object.error, 'deu ruim')
+
+      } else {
+
+        console.log("ok")
+      }
+    })
+  // .catch((error) => handleError(error))
+
+}
+window.onload = function onload() {
+  fecthItems(url);
+  createProductImageElement()
 
 
-
-
-window.onload = function onload() { };
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
+
   return img;
 }
 
@@ -26,30 +49,52 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// essa função cria a estrutura de hmtl dos produtos: img,title...
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', () => {
+      const endpointProducts = 'https://api.mercadolibre.com/items/$ItemID'
 
-  return section;
-}
+      fetchClickButton = endpointProducts => {
+        fetch(endpointProducts)
+          .then((response) => response.json)
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+          .then((object) => {
+            console.log(object);
+            if (object.error) {
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+              console.log(object.error, 'deu ruim')
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+            } else {
+
+              console.log("ok")
+            }
+          };
+
+        return section;
+      }
+
+      function getSkuFromProductItem(item) {
+        return item.querySelector('span.item__sku').innerText;
+      }
+
+      function cartItemClickListener(event) {
+
+
+      }
+
+      function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+        const li = document.createElement('li');
+        li.className = 'cart__item';
+        li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+        li.addEventListener('click', cartItemClickListener);
+        return li;
+      }
+
