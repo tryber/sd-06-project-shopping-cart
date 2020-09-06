573480dc57b569b4a3906c4f2ee972967c1b7910
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-06 01:07:34 -0300

    Add style to page

--- script.js
+++ script.js
@@ -48,13 +48,14 @@ function addItemToCart(event, sku) {
   if (event.target.className === 'item__add') fetchItem(sku);
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__price', `$${price}`));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   section.addEventListener('click', event => addItemToCart(event, sku));
   return section;
