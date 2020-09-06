c22aab42fbd283d1da47a7cb2b37d3e1c9713541
Danilo <dansantosalfa@gmail.com>
2020-09-06 12:49:45 -0300

    Arrumar o CC

--- script.js
+++ script.js
@@ -73,10 +73,10 @@ function createItemList() {
 function listClear() {
   const listCart = document.querySelector('.cart__items');
   listCart.innerHTML = '';
-  const buttonListClear = document.querySelector('.empty-cart');
-  buttonListClear.addEventListener('click', listClear);
 }
 
 window.onload = function onload() {
   createItemList();
+  const buttonListClear = document.querySelector('.empty-cart');
+  buttonListClear.addEventListener('click', listClear);
 };
