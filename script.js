794e8e82f82be0ecd39c3d51df6d6347decf6cfd
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-09-06 17:20:55 -0300

    Requirement 02 push 05 code climate fixes

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function addItemToCart(item) {
   const myCart = document.querySelector('.cart__items');
   myCart.appendChild(item);
@@ -40,22 +52,10 @@ const fetchSpecificMLItem = (id) => {
   fetch(specificMLItemEndpoint)
     .then(response => response.json())
     .then(object => addItemToCart(createCartItemElement(object)));
-}
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function fetchMLComputers() {
