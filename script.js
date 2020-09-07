9c51a4bc327712335fbb8a267d5a4eb672a743cf
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-07 17:59:41 -0300

    correcão exerc 5

--- script.js
+++ script.js
@@ -95,7 +95,6 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  //itemsCartPrice(salePrice);
   li.addEventListener('click', cartItemClickListener);
 
   return li;
