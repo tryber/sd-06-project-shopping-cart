ed89a4a43667c591635aaf034c56ffcad079390d
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-09-19 07:52:21 -0300

    requisito 2

--- script.js
+++ script.js
@@ -38,30 +38,40 @@ function createCartItemElement({ id: sku, Title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener(li));
   return li;
+}
+
+function clickButton(event) {
+  const itemId = event.path[1].childNodes[0].innerHTML;
+  const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((object) => {
+      const carItem = createCartItemElement(object);
+      const ol = document.querySelector('.car_items');
+      ol.appendChild(carItem);
+    });
 }
 
 function renderItems(arrayResults) {
   arrayResults.forEach((element) => {
     const newItem = createCartItemElement(element);
     const section = document.querySelector('.items');
-    section.appendChild(newItem).addEventListener;
+    section.appendChild(newItem).addEventListener;('click', clickButton);
   });
 }
 
 const fetchProductList = () => {
   fetch(url)
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((object) => {
       const result = object.results;
       renderItems(result);
-    })
-    
+    });
 };
 
 window.onload = function onload() {
   fetchProductList(url);
   
-  };
-  
+};
